ba3e62db37896203d4ab1ff1b0964e95212f08a8
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-10-18 14:16:00 +0700

    First iteration done

--- src/main/groovy/uy/kohesive/cuarentena/gradle/CuarentenaConfigurationExtension.groovy
+++ src/main/groovy/uy/kohesive/cuarentena/gradle/CuarentenaConfigurationExtension.groovy
@@ -3,11 +3,16 @@ package uy.kohesive.cuarentena.gradle
 class CuarentenaConfigurationExtension {
     String name
 
+    String kotlinVersion
+
     ScanMode scanMode = ScanMode.SAFE
     Boolean excludeKotlin = true
 
     List<String> excludePackages = Collections.emptyList()
     List<String> excludeClasses = Collections.emptyList()
+
+    Boolean verbose = false
+    String policyPackage = "uy.kohesive.cuarentena"
 
     CuarentenaConfigurationExtension(String name) {
         this.name = name
--- src/main/groovy/uy/kohesive/cuarentena/gradle/CuarentenaPlugin.groovy
+++ src/main/groovy/uy/kohesive/cuarentena/gradle/CuarentenaPlugin.groovy
@@ -14,7 +14,62 @@ class CuarentenaPlugin implements Plugin<Project> {
                 project.cuarentena.each { config ->
                     println "Generating allowances for $config.name (scan mode: $config.scanMode)"
 
-                    // TODO: implement
+                    // We need to exclude any transient kotlin dependencies first and form a jars list
+                    project.configurations.getByName(config.name) {
+                        exclude group: 'org.jetbrains.kotlin', module: 'kotlin-stdlib'
+                    }
+                    def jarsWithoutKotlin = project.configurations.getByName(config.name).collect { file -> file.path }
+
+                    // Then we need to add kotlin stdlib of a user-specified version and form a classpath
+                    def kotlinDependencyConfigName = "cuarentenaKotlin-${config.name}"
+                    project.configurations.create(kotlinDependencyConfigName)
+                    project.dependencies.add(kotlinDependencyConfigName, [group: 'org.jetbrains.kotlin', name: 'kotlin-stdlib', version: config.kotlinVersion])
+
+                    def classPathToVerify = (project.configurations.getByName(config.name) + project.configurations.getByName(kotlinDependencyConfigName)).collect { file ->
+                        new URL("file://${file.canonicalFile.absolutePath}")
+                    }
+
+                    if (config.verbose) {
+                        println 'JARs to be verified:'
+                        jarsWithoutKotlin.forEach { x -> println(x.toString()) }
+                        println()
+                        println 'Verification classpath:'
+                        classPathToVerify.forEach { x -> println(x.toString()) }
+                    }
+
+                    // We need to get rid of kotlin which is already in gradle's classpath. E.g., kotlin wrapper 4.2.1 contains kotlin 1.1.3
+                    def oldClassLoader    = Thread.currentThread().contextClassLoader as URLClassLoader
+                    def oldKotlinLessUrls = oldClassLoader.URLs.findAll { url -> !url.toString().contains("kotlin") }
+                    def tempClassLoader   = new URLClassLoader((classPathToVerify + oldKotlinLessUrls).toArray() as URL[])
+
+                    def outputDir  = new File(project.getBuildDir(), "generated-src/policy")
+                    def outputFile = new File(outputDir, "${config.policyPackage.replace('.', '/')}/${config.name}.ctena")
+                    outputFile.parentFile.mkdirs()
+
+                    Thread.currentThread().contextClassLoader = tempClassLoader
+                    try {
+                        def generatorArgs = [
+                            jarsWithoutKotlin,
+                            config.scanMode.toString(),
+                            config.excludePackages,
+                            config.excludeClasses,
+                            config.verbose,
+                            { String s -> println(s) }
+                        ].toArray()
+
+                        // Output policy
+                        def generator = tempClassLoader.loadClass("uy.kohesive.cuarentena.kotlin.JarAllowancesGenerator").newInstance(generatorArgs)
+                        generator.writePolicy(outputFile.path)
+                        println "\nDone writing policy file to ${outputFile.path}"
+
+                        def metaInfDir = new File(outputDir, "META-INF/cuarentena")
+                        metaInfDir.mkdirs()
+                        def metadataFile = new File(metaInfDir, "${config.name}.properties")
+                        metadataFile.write("resource=${config.policyPackage.replace('.', '/')}/${config.name}.ctena\n")
+                    }
+                    finally {
+                        Thread.currentThread().contextClassLoader = oldClassLoader
+                    }
                 }
             }
         }
